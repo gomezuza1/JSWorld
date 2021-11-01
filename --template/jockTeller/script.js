d9cc638cf52dcd200451d6110c4cdab83876efaf
gomezuza1 <javi_gom2002@hotmail.com>
2021-11-01 18:48:27 -0400

    add light and dark mode program

--- --template/jockTeller/script.js
+++ --template/jockTeller/script.js
@@ -1,6 +1,6 @@
 const button = document.getElementById("button");
 const audioElement = document.getElementById("audio");
-
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
 
 // use string --> VoiceRss Javascript SDK --- default library
 // var VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate:function(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){var a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw"The browser does not support the audio codec "+e.c}},_request:function(e){var a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;new Audio(t.responseText).play()}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest:function(e){var a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return"key="+(e.key||"")+"&src="+(e.src||"")+"&hl="+(e.hl||"")+"&v="+(e.v||"")+"&r="+(e.r||"")+"&c="+(a||"")+"&f="+(e.f||"")+"&ssml="+(e.ssml||"")+"&b64=true"},_detectCodec:function(){var e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR:function(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
@@ -79,10 +79,21 @@ async function getJoke() {
 
     }
 }
+// switch theme dinamically
+// document.documentElement ----> API 
+function switchTheme(event){
+    console.log(event.target.checked);
+    if(event.target.checked){
+        document.documentElement.setAttribute('data-theme', 'dark')
+    }else{
+        document.documentElement.setAttribute('data-theme', 'light')
+    }
+}
 
 // getJoke();
 // event listener
 button.addEventListener('click', getJoke);
 audioElement.addEventListener('ended' , toggleButton);
+toggleSwitch.addEventListener('change', switchTheme);
 
 
